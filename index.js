ab37c8f32b53ec1da34b6b6fd54dd25011605fb6
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-03 16:31:30 -0400

    Create browser window and test sidebar

---
+++ index.js
@@ -0,0 +1,8 @@
+const electron = require('electron'); 
+
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`); 
+});
